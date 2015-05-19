3b8292f1b4c6ce064887f53c18244e1108d1dd14
fitzsimk <kfitzsimmons11@gmail.com>
2015-05-19 16:50:12 -0700

    add pages

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var sampleSVG = d3.select("#viz");
-    .append("svg")
-    .attr("width", 100)
-    .attr("height", 100);
-
-sampleSVG.append("circle")
-    .style("stroke", "gray")
-    .style("fill", "white")
-    .attr("r", 40)
-    .attr("cx", 50)
-    .attr("cy", 50)
-    .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-    .on("mouseout", function(){d3.select(this).style("fill", "white");});
+require([
+  "/lib/codemirror", "/mode/htmlmixed/htmlmixed"
+], function(CodeMirror) {
+  CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    mode: "htmlmixed"
+  });
+});
