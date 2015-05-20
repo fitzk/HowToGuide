64c7082bc7fa075881650d7ef27aaa856eca70dc
fitzsimk <kfitzsimmons11@gmail.com>
2015-05-20 00:16:30 -0700

    add code mirror to main

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 require([
-  "/lib/codemirror", "/mode/htmlmixed/htmlmixed"
+  "CodeMirror/lib/codemirror", "CodeMirror/mode/htmlmixed/htmlmixed"
 ], function(CodeMirror) {
-  CodeMirror.fromTextArea(document.getElementById("code"), {
+  var editor1 = CodeMirror.fromTextArea(document.getElementById("code0"), {
+    lineNumbers: true,
+    mode: "htmlmixed"
+  });
+  
+  editor1.setOption("theme", 'solarized');
+  
+  CodeMirror.fromTextArea(document.getElementById("code1"), {
     lineNumbers: true,
     mode: "htmlmixed"
   });
 });
+
